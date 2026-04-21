68e63ab4725e4a2fe22d0f525c30fcb4b9578253
Giovanna Jesus Borba <giovannajesusborba23@gmail.com>
2026-04-21 16:21:57 -0300

    Criando classe Arma

---
+++ src/Arma.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+
+public abstract class Arma {
+    private int ataque;
+    private int chance;
+    private String nome;
+
+    public Arma(int ataque, int chance, String nome){
+        this.ataque = ataque;
+        this.chance = chance;
+        this.nome = nome;
+    }
+
+    public void Descricao(){
+        System.out.printf("%s \nDano = %d\nChance de acerto = %d%", this.nome, this.ataque, this.chance);
+    }
+
+    public void golpe (Criatura alvo){
+        Random rd = new Random();
+
+        int sorteio = rd.nextInt(100);
+        if(sorteio < this.chance){
+            alvo.tomaDano(this.ataque);
+        }
+        else{
+            System.out.println("Ataque falhou");
+        }
+    }
+}
